4b2113885ca0435c58f828171e28af45f2cad323
shossain112 <41022765+shossain112@users.noreply.github.com>
2018-07-16 15:00:15 -0400

    Added Palindrome Project (1st half) through File Upload

---
+++ CodingChallenges/CodingChallenges/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+   public static class Program
+    {
+        public static bool IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (!input[i].Equals(input[input.Length - i - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CodingChallenges/UnitTestProject1/PDromeTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenges;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PDromeTest
+    {
+        [TestMethod]
+        public void PTest1()
+        {
+            string a = "noon";
+            bool expectedValue = true;
+            bool actualValue;
+            actualValue = Program.IsPalindrome(a);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        [TestMethod]
+        public void PTest2()
+        {
+            string b = "2112";
+            bool expectedValue = true;
+            bool actualValue;
+            actualValue = Program.IsPalindrome(b);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
